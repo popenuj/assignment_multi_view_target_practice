96831f2f618a62d40532b91b9c68e39a8f75f6b3
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-27 08:17:52 -0800

    verified all views are displayed properly

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@ var TargetPractice = angular.module("TargetPractice", ['ui.router']);
 
 TargetPractice.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('parent',
+      {
+        url: '/',
+        views: {
+          '': {
+            template: "Targeting unnamed view from parent state."
+          },
+          'named-parent': {
+            template: "Targeting named view from parent state."
+          },
+          'main-header': {
+            template: 'Targeting header from parent state.'
+          }
+        }
+      });
 
-TargetPractice.factory('_', ['$window', function($window) {
-  return $window._;
-}]);
+  $urlRouterProvider.otherwise('/');
+});
