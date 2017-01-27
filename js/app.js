7eafc91d267992649436763e8d97cee8e6ed08e7
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-27 11:41:38 -0500

    children!

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ TargetPractice.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('parent',
       {
-        url: '/',
+        url: 'parent',
         views: {
           '': {
             template: "Targeting unnamed view from parent state."
@@ -13,12 +13,38 @@ TargetPractice.config(function($stateProvider, $urlRouterProvider) {
           'named-parent': {
             template: "Targeting named view from parent state."
           },
-          'main-header': {
+          'main-header@': {
             template: 'Targeting header from parent state.'
           }
         }
       })
-      .state('parent.child');
+      .state('parent.child',
+      {
+        url: 'child',
+        views: {
+          '@' :{
+            templateUrl: "/js/templates/child.html"
+          },
+
+          '@parent.child' :{
+            template: 'Targeting unnamed child view from child state'
+          },
+
+          "named-child@parent.child" :{
+            template: 'Targeting named child view from child state'
+          },
+
+          "named-parent@" :{
+            template: 'Targeting named parent view from child state'
+          },
+
+          "main-header@" :{
+            template: 'Targeting header from child state'
+          }
+        }
+      }
+
+        );
 
   $urlRouterProvider.otherwise('/');
 });
